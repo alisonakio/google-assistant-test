656002b3ba48c7587703c597198b83a7096c5589
Alison <alison.miazaki@gmail.com>
2020-01-13 13:50:30 -0300

    setting up express

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const { promisify } = require('util')
+const webhook = require('webhook');
 
 const app = express()
 app.use(bodyParser.json())
+
+app.post('/', (req,res)=>{
+    console.log('Default route');
+    res.send('Google Assistant Test');
+})
 
 const startServer = async () => {
   const port = process.env.SERVER_PORT || 3000

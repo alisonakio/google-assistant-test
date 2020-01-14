9a82f1c6c8daa86ff1a1755205c1d4c807503a90
Alison <alison.miazaki@gmail.com>
2020-01-14 15:38:51 -0300

    logging email

--- webhook.js
+++ webhook.js
@@ -1,30 +1,36 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-const {actionssdk, SignIn} = require('actions-on-google');
-const app = actionssdk({debug: true});
+const {actionssdk, SignIn} = require('actions-on-google')
+const app = actionssdk({debug: true})
 
 app.intent('actions.intent.MAIN', (conv) => {
   conv.ask('Hi!');
-});
+})
 
 app.intent('actions.intent.TEXT', (conv) => {
     conv.ask(new SignIn('Vamos pegar seus dados'));
-});
+})
 
 app.intent('actions.intent.SIGN_IN', (conv, params, signin) => {
     if (signin.status === 'OK') {
-      const payload = JSON.stringify(conv.user.profile.payload);
-      conv.ask(`I got your account details, ${payload}. What do you want to do next?`);
+        conv.ask('HAHAHAHAHAHA!')
+        let email = conv.user
+        console.log(email)
+        conv.ask(`${email}`)
+    //   const payload = JSON.stringify(conv.user.profile.payload);
+    //   conv.ask(`I got your account details, ${payload}. What do you want to do next?`);
     } else {
-      conv.ask(`I won't be able to save your data, but what do you want to do next?`);
+        console.log('num foi')
+        conv.ask(':(')
+    //   conv.ask(`I won't be able to save your data, but what do you want to do next?`);
     }
   })
 
 router.post('/',app)
 
 router.get('/',(req,res)=>{
-    res.send('webhook');
+    res.send('webhook')
 })
 
-module.exports = router;
+module.exports = router

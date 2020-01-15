7bc6714cd21f5087a6e8a4e9f23b11b8ce367b99
akioali <alison.miazaki@itau-unibanco.com.br>
2020-01-15 15:56:16 -0300

    client Id

--- webhook.js
+++ webhook.js
@@ -1,36 +1,40 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const {actionssdk, SignIn} = require('actions-on-google')
-const app = actionssdk({debug: true})
+const { actionssdk, SignIn } = require("actions-on-google");
+const app = actionssdk({
+  debug: true,
+  clientId:
+    "194996161787-h383lk60f9kn59teje3vlofemludn4r8.apps.googleusercontent.com"
+});
 
-app.intent('actions.intent.MAIN', (conv) => {
-  conv.ask('Hi!');
-})
+app.intent("actions.intent.MAIN", conv => {
+  conv.ask("Hi!");
+});
 
-app.intent('actions.intent.TEXT', (conv) => {
-    conv.ask(new SignIn());
-})
+app.intent("actions.intent.TEXT", conv => {
+  conv.ask(new SignIn("Vamos pegar seus dados"));
+});
 
-app.intent('actions.intent.SIGN_IN', (conv, params, signin) => {
-    if (signin.status === 'OK') {
-        conv.ask('HAHAHAHAHAHA!')
-        let email = conv.user
-        console.log(email)
-        conv.ask(`${email}`)
+app.intent("actions.intent.SIGN_IN", (conv, params, signin) => {
+  if (signin.status === "OK") {
+    conv.ask("HAHAHAHAHAHA!");
+    let email = conv.user;
+    console.log(email);
+    conv.ask(`${email}`);
     //   const payload = JSON.stringify(conv.user.profile.payload);
     //   conv.ask(`I got your account details, ${payload}. What do you want to do next?`);
-    } else {
-        console.log('num foi')
-        conv.ask(':(')
+  } else {
+    console.log("num foi");
+    conv.ask(":(");
     //   conv.ask(`I won't be able to save your data, but what do you want to do next?`);
-    }
-  })
+  }
+});
 
-router.post('/',app)
+router.post("/", app);
 
-router.get('/',(req,res)=>{
-    res.send('webhook')
-})
+router.get("/", (req, res) => {
+  res.send("webhook");
+});
 
-module.exports = router
+module.exports = router;
